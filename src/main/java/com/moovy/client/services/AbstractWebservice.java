8868b95ccc569ed6f0ff95a8f727eb239aea00af
Bruno Buiret <bruno.b@apps-panel.com>
2016-10-10 09:32:49 +0200

    Fichiers de base

---
+++ src/main/java/com/moovy/client/services/AbstractWebservice.java
@@ -0,0 +1,10 @@
+package com.moovy.client.services;
+
+/**
+ * @author Thomas Arnaud (thomas.arnaud@etu.univ-lyon1.fr)
+ * @author Bruno Buiret (bruno.buiret@etu.univ-lyon1.fr)
+ * @author Alexis Rabilloud (alexis.rabilloud@etu.univ-lyon1.fr)
+ */
+public abstract class AbstractWebservice
+{
+}
